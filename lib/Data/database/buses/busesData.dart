0357e43d0d91b25efc3264e277a867cde5f2c728
Tushar Mishra <tushar@bitcs.in>
2021-01-13 09:32:20 +0530

    data

--- lib/Data/database/buses/busesData.dart
+++ lib/Data/database/buses/busesData.dart
@@ -5,9 +5,9 @@ class BusesData{
   static List<Bus> data=[
     Bus('MH-2102',['Old delhi', 'Delhi gate','Dilshad Colony'],'This is a non AC regular bus which origiates from Delhi gate...'),
     Bus('MH-2302',['Darya Ganj', ' GPO Kashmiri Gate','Jama Masjid I'],'This is a non AC regular bus which origiates from Delhi gate...'),
-    Bus('MH-2102',[' Subhash Park I', 'Delhi gate','Ram Lila Ground Red Fort'],'This is a non AC regular bus which origiates from Delhi gate...'),
-    Bus('MH-2102',['Dariya Ganj Golcha Cinema', 'Dr Ambedkar Stadium I','ITO BSZ Marg I'],'This is a non AC regular bus which origiates from Delhi gate...'),
-    Bus('MH-2102',[' HASAN PUR VILL', 'K. K. XING','NEW RAJDHANI ENCLAVE'],'This is a non AC regular bus which origiates from Delhi gate...'),
+    Bus('MH-2354',[' Subhash Park I', 'Delhi gate','Ram Lila Ground Red Fort'],'This is a non AC regular bus which origiates from Delhi gate...'),
+    Bus('MH-6844',['Dariya Ganj Golcha Cinema', 'Dr Ambedkar Stadium I','ITO BSZ Marg I'],'This is a non AC regular bus which origiates from Delhi gate...'),
+    Bus('MH-3434',[' HASAN PUR VILL', 'K. K. XING','NEW RAJDHANI ENCLAVE'],'This is a non AC regular bus which origiates from Delhi gate...'),
 
   ];
 
